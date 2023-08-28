5e65f454f139f2f089fc9b3f2f9ff2a3ae4b0736
David Benkner <d.benkner@gmail.com>
2023-08-28 09:54:29 -0400

    Add project files.

---
+++ EnityFrameWorkDbFirst/Models/PrsDbContext.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace EnityFrameWorkDbFirst.Models;
+
+public partial class PrsDbContext : DbContext
+{
+    public PrsDbContext()
+    {
+    }
+
+    public PrsDbContext(DbContextOptions<PrsDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    public virtual DbSet<Request> Requests { get; set; }
+
+    public virtual DbSet<RequestLine> RequestLines { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    public virtual DbSet<Vendor> Vendors { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("server=localhost\\sqlexpress;database=PrsDb;trusted_connection=true;trustServerCertificate=true;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Products__3214EC07E8510D31");
+
+            entity.HasIndex(e => e.PartNbr, "UQ__Products__DAFC0C1E39B033D4").IsUnique();
+
+            entity.Property(e => e.Name)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+            entity.Property(e => e.PartNbr)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+            entity.Property(e => e.PhotoPath)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.Price).HasColumnType("decimal(11, 2)");
+            entity.Property(e => e.Unit)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.Vendor).WithMany(p => p.Products)
+                .HasForeignKey(d => d.VendorId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Products__Vendor__2D27B809");
+        });
+
+        modelBuilder.Entity<Request>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Requests__3214EC07095E65E6");
+
+            entity.Property(e => e.DelieveryMode)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasDefaultValueSql("('Pickup')");
+            entity.Property(e => e.Description)
+                .HasMaxLength(80)
+                .IsUnicode(false);
+            entity.Property(e => e.Justification)
+                .HasMaxLength(80)
+                .IsUnicode(false);
+            entity.Property(e => e.RejectionReason)
+                .HasMaxLength(80)
+                .IsUnicode(false);
+            entity.Property(e => e.Status)
+                .HasMaxLength(10)
+                .IsUnicode(false)
+                .HasDefaultValueSql("('NEW')");
+            entity.Property(e => e.Total).HasColumnType("decimal(11, 2)");
+
+            entity.HasOne(d => d.User).WithMany(p => p.Requests)
+                .HasForeignKey(d => d.UserId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Requests__UserId__32E0915F");
+        });
+
+        modelBuilder.Entity<RequestLine>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__RequestL__3214EC07252BDAD6");
+
+            entity.Property(e => e.Quantity).HasDefaultValueSql("((1))");
+
+            entity.HasOne(d => d.Product).WithMany(p => p.RequestLines)
+                .HasForeignKey(d => d.ProductId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__RequestLi__Produ__36B12243");
+
+            entity.HasOne(d => d.Request).WithMany(p => p.RequestLines)
+                .HasForeignKey(d => d.RequestId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__RequestLi__Reque__35BCFE0A");
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Users__3214EC07239D2CAB");
+
+            entity.HasIndex(e => e.Username, "UQ__Users__536C85E4AA5EB2DD").IsUnique();
+
+            entity.Property(e => e.Email)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.Firstname)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+            entity.Property(e => e.LastName)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+            entity.Property(e => e.Password)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+            entity.Property(e => e.Phone)
+                .HasMaxLength(12)
+                .IsUnicode(false);
+            entity.Property(e => e.Username)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Vendor>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Vendors__3214EC073C9E3833");
+
+            entity.HasIndex(e => e.Code, "UQ__Vendors__A25C5AA7B58150F2").IsUnique();
+
+            entity.Property(e => e.Address)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+            entity.Property(e => e.City)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+            entity.Property(e => e.Code)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+            entity.Property(e => e.Email)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.Name)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+            entity.Property(e => e.Phone)
+                .HasMaxLength(12)
+                .IsUnicode(false);
+            entity.Property(e => e.State)
+                .HasMaxLength(2)
+                .IsUnicode(false);
+            entity.Property(e => e.Zip)
+                .HasMaxLength(5)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ EnityFrameWorkDbFirst/Program.cs
@@ -0,0 +1,12 @@
+﻿using EnityFrameWorkDbFirst.Models;
+
+
+var _context = new PrsDbContext();
+
+var users = _context.Users.ToList();
+
+foreach(var user in users)
+{
+    Console.WriteLine($"{user.Firstname} {user.LastName}");
+}
+
